b6067698165bb8bb1cff12348c4ad27f3fbf5b99
TumikoSakayama <glee310597@gmail.com>
2026-02-15 14:29:55 -0600

    Initial logic for the habits and the file handling of JSON

---
+++ habits.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+class Habit:
+    def __init__(self, name, category, weekly_goal=7, created_at=None, completion_date=None):
+        self.name = name.strip()
+        self.category = category.strip()
+        self.weekly_goal = int(weekly_goal)
+        self.created_at = created_at if created_at else datetime.now().strftime("%Y-%m-%d")
+        self.completion_date = completion_date if completion_date else []
+
+    def to_dict(self):
+        return{
+            "name": self.name,
+            "category": self.category,
+            "weekly_goal": self.weekly_goal,
+            "created_at": self.created_at,
+            "completion_date": self.completion_date
+        }
+
+    @staticmethod
+    def from_dict(self):
+        return Habit{
+            data['name'],
+            data['category'],
+            data['weekly_goal', 7],
+            data['created_at'],
+            data['completion_date']
+        }
+
+    
+
+        
---
+++ logichandler.py
@@ -0,0 +1,82 @@
+import json
+import os
+from datetime import datetime
+from habits import Habit
+
+class HabitHandler:
+    def __init__(self):
+        self.habits = []
+        self.current_file = None
+
+    def new_collection(self, file_path):
+        try:
+            with open(file_path, 'w') as f:
+                json.dump([], f)
+            self.current_file = file_path
+            self.habits = []
+            return True, "New collection created!"
+        except Exception as e:
+            return False, f"Failed to create file: {str(e)}"
+
+    def load_habits(self, file_path):
+        try:
+            if not os.path.exists(file_path):
+                return False, "File does not exist"
+
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+                if not isinstance(data, list):
+                    return False, "Corrupted file format."
+                self.habits = [Habit.from_dict(item) for item in data]
+            self.current_file = file_path
+            return True, f"Loaded: {len(self.habits)} habits"
+        except json.JSONDecodeError:
+            return False, "File is not a valid JSON."
+        except Exception as e:
+            return False, str(e)
+
+    def add_habit(self, name, category, goal):
+        if not name or category:
+            return False, "Name nor category cannot be empty."
+        
+        if any(h.name.lower() == name.strip().lower() for h in self.habits):
+            return False, f"A habit named {name} already exists."
+
+        try:
+            g_val = int(goal)
+            if not (1 <= g_val <=7):
+                return False, "Goal must be between 1 to 7 days per week." 
+        except ValueError:
+            return False, "Goal must be a whole (integer) number."
+
+        new_h = Habit(name, category, g_val)
+        self.habits.append(new_h)
+        return self.save_to_file()
+
+    def toggle_completion(self, habit_name, date_str=None):
+        if not date_str:
+            date_str = datetime.now().strftime("%Y-%m-%d")
+
+        for h in self.habits:
+            if h.name == habit_name:
+                if date_str in h.completion_date:
+                    h.completion_date.remove(date_str)
+                    msg = "Entry removed."
+                else:
+                    h.completion_date.append(date_str)
+                    msg = "Entry added."
+                return self.save_to_file(msg)
+        
+        return False, "Habit not found."
+
+    def save_to_file(self, success_msg="Changes saved!"):
+        if not self.current_file:
+            return False, "No file loaded, please load a file."
+
+        try:
+            data_to_save = [h.to_dict() for h in self.habits]
+            with open(self.current_file, 'w') as f:
+                json.dump(data_to_save, f, indent=4)
+            return True, success_msg
+        except Exception as e:
+            return False, f"Disk Error: {str(e)}"
