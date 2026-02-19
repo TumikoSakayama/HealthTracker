8c5f75e69367b4c7042acfd9ce5b25ba1e183ad5
TumikoSakayama <glee310597@gmail.com>
2026-02-18 20:23:49 -0600

    Corrected some logic and import at main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import customtkinter as ctk
 from fitness_app import FitnessTrackerApp
 
-if name == __"main"__:
-  ctk.set_apperance_mode("Dark")
-  app = FitnessTrakcerApp()
+if name == "__main__":
+  ctk.set_appearance_mode("Dark")
+  app = FitnessTrackerApp()
   app.mainloop()
